e8488ac3de66f331db01ef3ca084ecad26f4a3d2
ingNicolasudec <nicolasgarcia1996@yahoo.com>
2017-05-23 17:58:36 -0500

    Add files via upload
    Agrego consultar

---
+++ consultar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace colegio1
+{
+    public partial class consultar : colegio1.plantilla
+    {
+        public consultar()
+        {
+            InitializeComponent();
+        }
+    }
+}
