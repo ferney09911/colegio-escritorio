d55dc067afd6ebaeda26ff2be5967b8545a83c79
ferney09911 <ferneyvillalba@hotmail.com>
2017-05-24 09:23:15 -0500

    Add files via upload

---
+++ vision.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace colegio1
+{
+    public partial class vision : Form
+    {
+        public vision()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
